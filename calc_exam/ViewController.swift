2d0d526b9b053c829fe2d203c69ace259bd7be02
hwanu <sossont1@gmail.com>
2020-12-31 00:30:45 +0900

    make cal

--- calc_exam/ViewController.swift
+++ calc_exam/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
-    
     @IBOutlet weak var btn1: UIButton!
-    
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn10: UIButton!
+    @IBOutlet weak var btn11: UIButton!
     
     override func viewDidLoad() {   // 화면이 처음 실행될 때 실행되는 코드들.
         
@@ -31,6 +39,16 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         btn1.layer.cornerRadius = btn1.bounds.width / 2
+        btn2.layer.cornerRadius = btn1.bounds.width / 2
+        btn3.layer.cornerRadius = btn1.bounds.width / 2
+        btn4.layer.cornerRadius = btn1.bounds.width / 2
+        btn5.layer.cornerRadius = btn1.bounds.width / 2
+        btn6.layer.cornerRadius = btn1.bounds.width / 2
+        btn7.layer.cornerRadius = btn1.bounds.width / 2
+        btn8.layer.cornerRadius = btn1.bounds.width / 2
+        btn9.layer.cornerRadius = btn1.bounds.width / 2
+        btn10.layer.cornerRadius = btn1.bounds.height / 2
+        btn11.layer.cornerRadius = btn1.bounds.width / 2
     }
     
     
