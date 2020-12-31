206bcff19c0e273a7ebe18a1af71ed68f0600486
hwanu <sossont1@gmail.com>
2020-12-31 22:01:21 +0900

    iPhone calculator clone coding

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func sayHelloTo(_ name:String, _ age:Int){
+    print("Hello \(name), you're \(age) years old")
+}
+
+sayHelloTo("HwanWoo",24)
+
+func addTwonumbers(_ x:Int,_ y:Int)->Int{
+    
+return x+y
+    
+}
+
+print(addTwonumbers(5, 6))
