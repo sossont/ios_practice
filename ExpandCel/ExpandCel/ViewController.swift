a68acc2ed56a651374a09d241f0962953811b05b
hwanu <sossont1@gmail.com>
2021-01-05 17:42:51 +0900

    복습

--- ExpandCel/ExpandCel/ViewController.swift
+++ ExpandCel/ExpandCel/ViewController.swift
@@ -11,7 +11,9 @@ class ExpandCell:UITableViewCell{
     
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    
 }
+
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     // 저 두가지를 선언해야 테이블 부 관련한 메소드들을 사용가능하다.
@@ -28,42 +30,56 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         var isExpand: Bool
     }
     
-    var dataModels = [ExpandDataModel]()    // 구조체 배열 선언.
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let textArray = ["short text", "long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text",
-                         "short text", "long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text",
-                         "short text", "long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text"
-        ]
-        for (_,value) in textArray.enumerated(){
-            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataModels.count
-    }
-
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "expandCel_ID", for: indexPath) as! ExpandCell
-        // 타입 캐스팅 필요함
         
         cell.descriptionLabel.text = dataModels[indexPath.row].description
         
-        if dataModels[indexPath.row].isExpand == true{//확장이 필요한 경우
+        if dataModels[indexPath.row].isExpand == true
+        {
             cell.descriptionLabel.numberOfLines = 0
-            // 0 이면 글자 줄 수 만큼 표현해라 라는 뜻.
         }
         else
         {
             cell.descriptionLabel.numberOfLines = 1
         }
+        cell.selectionStyle = .none
         
-        return cell
+            return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {   // Row 개수 반환하는 함수.
+        
+        return dataModels.count
+        
+    }
+    var dataModels = [ExpandDataModel]()    // 구조체 배열 선언.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let textArray = ["short Text",
+            "long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long ",
+            "short Text",
+                "long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long ",
+            "short Text",
+                "long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long ",
+            "short Text",
+            "long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long "
+        ]   // 실제 서버에서 오는 데이터일것이다. 실무에서는
+        
+        for (_,value) in textArray.enumerated(){
+            
+            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 클릭하면 확장하거나 축소해야한다 -> isExpand 값이 바뀌어야한다.
+        dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
     }
 }
-
