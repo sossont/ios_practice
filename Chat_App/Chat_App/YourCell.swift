a02991805183eab01ef853eb36b849456c1a6c77
hwanu <sossont1@gmail.com>
2021-01-23 00:18:36 +0900

    채팅어플 만들기

---
+++ Chat_App/Chat_App/YourCell.swift
@@ -0,0 +1,23 @@
+//
+//  YourCell.swift
+//  Chat_App
+//
+//  Created by 정환우 on 2021/01/22.
+//
+
+import UIKit
+
+class YourCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
